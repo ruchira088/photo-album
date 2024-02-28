b4f3a9191e17d994ae56a54f6198a9036e39bc67
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-02-28 19:36:10 +1100

    Adding the user ID to the album response

--- src/main/java/com/ruchij/photo/album/web/controllers/responses/AlbumResponse.java
+++ src/main/java/com/ruchij/photo/album/web/controllers/responses/AlbumResponse.java
@@ -4,8 +4,13 @@ import com.ruchij.photo.album.daos.album.Album;
 
 import java.util.Optional;
 
-public record AlbumResponse(String id, String name, Optional<String> description) {
+public record AlbumResponse(String id, String userId, String name, Optional<String> description) {
 	public static AlbumResponse from(Album album) {
-		return new AlbumResponse(album.getId(), album.getName(), album.getDescription());
+		return new AlbumResponse(
+			album.getId(),
+			album.getUser().getId(),
+			album.getName(),
+			album.getDescription()
+		);
 	}
 }
