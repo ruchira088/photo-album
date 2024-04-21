f773c0bbd8ddbecf1df660b64b603b0a9c36be0b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-04-21 10:18:50 +0530

    Adding usage entries to existing users

--- src/main/resources/db/migration/V006__create_usage_table.sql
+++ src/main/resources/db/migration/V006__create_usage_table.sql
@@ -8,4 +8,7 @@ CREATE TABLE usage
 
     PRIMARY KEY (user_id),
     CONSTRAINT fk_usage_user_id FOREIGN KEY (user_id) REFERENCES api_user (id)
-)
+);
+
+INSERT INTO usage (user_id, last_updated_at, photo_count, album_count, bytes_used)
+    SELECT id, NOW(), 0, 0, 0 FROM api_user;
